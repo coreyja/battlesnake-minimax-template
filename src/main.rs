54afe0847935d392c70e3a3c1dc2213548a19169
Corey Alexander <coreyja@gmail.com>
2022-03-19 13:36:12 -0400

    Get SnakeInfo endpoint setup like I want it

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,56 @@
-use axum::{routing::get, Router};
-use std::net::SocketAddr;
+use axum::{response::IntoResponse, routing::get, Json, Router};
+use serde::Serialize;
+use std::{env, net::SocketAddr};
+use tracing::{info, info_span, instrument};
+use tracing_subscriber::EnvFilter;
+
+#[derive(Serialize)]
+pub struct SnakeInfo {
+    apiversion: String,
+    author: Option<String>,
+    color: Option<String>,
+    head: Option<String>,
+    tail: Option<String>,
+    version: Option<String>,
+}
 
 #[tokio::main]
 async fn main() {
+    if env::var("RUST_LOG").is_err() {
+        env::set_var("RUST_LOG", "info");
+    }
+
     // initialize tracing
-    tracing_subscriber::fmt::init();
+    tracing_subscriber::fmt()
+        .with_env_filter(EnvFilter::from_default_env())
+        .pretty()
+        .init();
 
     // build our application with a route
     let app = Router::new()
         // `GET /` goes to `root`
-        .route("/", get(root));
+        .route("/", get(snake_info));
 
     // run our app with hyper
     // `axum::Server` is a re-export of `hyper::Server`
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
-    tracing::debug!("listening on {}", addr);
+
+    info!("listening on {}", addr);
+
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
         .await
         .unwrap();
 }
 
-// basic handler that responds with a static string
-async fn root() -> &'static str {
-    "Hello, World!"
+#[instrument]
+async fn snake_info() -> impl IntoResponse {
+    Json(SnakeInfo {
+        apiversion: "1.0".to_string(),
+        author: None,
+        color: None,
+        head: None,
+        tail: None,
+        version: None,
+    })
 }
