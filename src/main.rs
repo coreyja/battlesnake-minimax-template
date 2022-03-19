a0cae3ca01f0c8544dbac54e75abb3acc2135982
Corey Alexander <coreyja@gmail.com>
2022-03-19 14:21:42 -0400

    Always move right, we can play real games with this now

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,9 @@ use axum::{
     routing::{get, post},
     Json, Router,
 };
-use battlesnake_game_types::wire_representation::Game;
+use battlesnake_game_types::{types::Move, wire_representation::Game};
 use serde::Serialize;
+use serde_json::json;
 use std::{env, net::SocketAddr};
 use tracing::{info, info_span, instrument};
 use tracing_subscriber::EnvFilter;
@@ -37,7 +38,8 @@ async fn main() {
         // `GET /` goes to `root`
         .route("/", get(snake_info))
         .route("/start", post(game_start))
-        .route("/end", post(game_end));
+        .route("/end", post(game_end))
+        .route("/move", post(make_move));
 
     // run our app with hyper
     // `axum::Server` is a re-export of `hyper::Server`
@@ -56,7 +58,7 @@ async fn snake_info() -> impl IntoResponse {
     info!("Hit Snake Info Route");
 
     Json(SnakeInfo {
-        apiversion: "1.0".to_string(),
+        apiversion: "1".to_string(),
         author: None,
         color: None,
         head: None,
@@ -77,4 +79,11 @@ async fn game_end(Json(game): Json<Game>) -> impl IntoResponse {
     info!("Hit End Route");
 
     StatusCode::OK
+}
+
+#[instrument(skip(game), fields(game_id = %game.game.id))]
+async fn make_move(Json(game): Json<Game>) -> impl IntoResponse {
+    info!("Hit Make Move Route");
+
+    Json(json!({ "move": Move::Right.to_string() }))
 }
